6bb7e74b918ad872af877036d5876a2bc3528285
Lemdy <58974140+Lunarelly@users.noreply.github.com>
2021-02-23 14:31:40 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+<p align="center">
+	<b>Glowstone: Premium Minecraft: PE software</b>
+</p>
+
+## Installation
+- Download PHP 7 libaries
+- Download and unzip core
+- Run via ./start.sh
+
+## Donate
+- QIWI: qiwi.com/n/LEMDY
+
+## Licensing information
+Glowstone are not affiliated with Mojang. All brands and trademarks belong to their respective owners.
---
+++ start.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+DIR="$(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+cd "$DIR"
+
+while getopts "p:f:l" OPTION 2> /dev/null; do
+	case ${OPTION} in
+		p)
+			PHP_BINARY="$OPTARG"
+			;;
+		f)
+			POCKETMINE_FILE="$OPTARG"
+			;;
+		l)
+			DO_LOOP="yes"
+			;;
+		\?)
+			break
+			;;
+	esac
+done
+
+if [ "$PHP_BINARY" == "" ]; then
+	if [ -f ./bin/php7/bin/php ]; then
+		export PHPRC=""
+		PHP_BINARY="./bin/php7/bin/php"
+	elif [[ ! -z $(type php) ]]; then
+		PHP_BINARY=$(type -p php)
+	else
+		echo "PHP 7 Libaries not found"
+		exit 1
+	fi
+fi
+
+if [ "$POCKETMINE_FILE" == "" ]; then
+	if [ -f ./src/pocketmine/PocketMine.php ]; then
+		POCKETMINE_FILE="./src/pocketmine/PocketMine.php"
+	else
+		echo "Glowstone not found"
+		exit 1
+	fi
+fi
+
+LOOPS=0
+
+set +e
+
+if [ "$DO_LOOP" == "yes" ]; then
+	while true; do
+		if [ ${LOOPS} -gt 0 ]; then
+			echo "Restarted $LOOPS times"
+		fi
+		"$PHP_BINARY" "$POCKETMINE_FILE" $@
+		echo "Stop - CTRL+C"
+		echo ""
+		sleep 5
+		((LOOPS++))
+	done
+else
+	exec "$PHP_BINARY" "$POCKETMINE_FILE" $@
+fi
