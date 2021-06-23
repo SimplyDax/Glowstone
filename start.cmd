050241903fb5726cd033f04ea4e58bd1477f3ff0
Lunarelly <58974140+Lunarelly@users.noreply.github.com>
2021-06-24 00:28:41 +0300

    added start.cmd for Windows

---
+++ start.cmd
@@ -0,0 +1,36 @@
+@echo off
+TITLE Glowstone server software for Minecraft: Pocket Edition
+cd /d %~dp0
+
+if exist bin\php\php.exe (
+	set PHPRC=""
+	set PHP_BINARY=bin\php\php.exe
+) else (
+	set PHP_BINARY=php
+)
+
+if exist Glowstone*.phar (
+	set POCKETMINE_FILE=Glowstone*.phar
+) else (
+	if exist PocketMine-MP.phar (
+		set POCKETMINE_FILE=PocketMine-MP.phar
+	) else (
+	    if exist src\pocketmine\PocketMine.php (
+	        set POCKETMINE_FILE=src\pocketmine\PocketMine.php
+		) else (
+			if exist Glowstone.phar (
+				set POCKETMINE_FILE=Glowstone.phar
+			) else (
+		        echo "[ERROR] Couldn't find a valid Glowstone installation."
+		        pause
+		        exit 8
+		    )
+	    )
+	)
+)
+
+if exist bin\mintty.exe (
+	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "Glowstone" -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
+) else (
+	%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
+)
